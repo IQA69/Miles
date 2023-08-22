451277d25a5638e8c4e74be83b184e853656837f
IQA69 <charlesdward@mail.ru>
2023-08-22 23:31:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int prise = 15652; // стоимость билета
+            int oneBonus = 20; // стоимость бонуса
+            int bonus = prise / oneBonus; // количество бонусов
+            System.out.println("Количество бонусных миль - " + bonus);
+    }
+}
